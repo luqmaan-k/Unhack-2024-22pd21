f94c31fecac0b4b450e0b316efce93e5b6f749ea
Luqmaan-k <2luqmaank2004@gmail.com>
2024-12-07 17:01:42 +0530

    Most working

--- sol.py
+++ sol.py
@@ -21,14 +21,12 @@ def checkIfNeedCooldown(machine,steps):
 
 def findWafer(machine,wafers_unprocessed,steps):
   step_ids = []
-  print(wafers_unprocessed)
   for step in steps:
     step_ids.append(step['id'])
   for idx,wafer in enumerate(wafers_unprocessed):
-    if (machine['step_id'] in  wafer['processing_times']) :
-      if (wafer['processing_times'][machine['step_id']] > 0):
-        print("Wafer : ",wafer)
-        print("Allocated to : ",machine)
+    if (machine['step_id'] in  wafer[0]['processing_times']) :
+        # print("Wafer : ",wafer)
+        # print("Allocated to : ",machine)
         return True,wafers_unprocessed.pop(idx)
   return False,None
 
@@ -37,26 +35,29 @@ def machineProcess(processing_machine):
   modified_machine = processing_machine[0]
   available_time   = processing_machine[1]
   modified_wafer   = processing_machine[2]
-  start_time = available_time
   machine_stage = modified_machine['step_id']
 
   modified_machine['n'] = modified_machine['n'] - 1
   # if n == 0?
   for key in modified_machine['initial_parameters']:
     modified_machine['initial_parameters'][key] += modified_machine["fluctuation"][key]
-  available_time  = available_time + modified_wafer["processing_times"][machine_stage]
-
-  print("-------",machine_stage)
-  print("++++++++++++",modified_wafer['processing_times'][machine_stage])
-  del modified_wafer['processing_times'][machine_stage]
+  if(modified_wafer[1] > available_time):
+    # print("  Ive Run -------------------------------")
+    start_time = modified_wafer[1]
+    available_time  = modified_wafer[1] + modified_wafer[0]["processing_times"][machine_stage]
+  else :
+    # print("  Ive Run +++++++++++++++++++++++++++++++",modified_wafer[1],available_time)
+    start_time = available_time
+    available_time  = available_time + modified_wafer[0]["processing_times"][machine_stage]
+  del modified_wafer[0]['processing_times'][machine_stage]
   # Maybe Handle dependecy here???
 
-  print("Processed :-")
-  print("Modified Machine : ",modified_machine)
-  print("Modified Wafer",modified_wafer)
-  print(available_time)
-
-  mw_schedule = writeSchedulePlan(modified_wafer['wafer_id'],machine_stage,modified_machine['machine_id'],start_time,available_time)
+  # print("Processed :-")
+  # print("Modified Machine : ",modified_machine)
+  # print("Modified Wafer",modified_wafer[0])
+  # print(available_time)
+  modified_wafer[1] = available_time
+  mw_schedule = writeSchedulePlan(modified_wafer[0]['wafer_id'],machine_stage,modified_machine['machine_id'],start_time,available_time)
 
   return modified_machine,available_time,modified_wafer,mw_schedule
 
@@ -64,7 +65,8 @@ def machineCooldown(cooldown_machine,allmachines):
   # Returns the modified machines after cooldown
   modified_machine = cooldown_machine[0]
   available_time   = cooldown_machine[1]
-  temp_machine_id = cooldown_machine['machine_id']
+  print(cooldown_machine)
+  temp_machine_id = cooldown_machine[0]['machine_id']
 
   available_time += modified_machine['cooldown_time']
 
@@ -83,9 +85,7 @@ def scheduleWafers(steps,machines,wafers):
   # Covert the wafers to their wafer ids and store in an list containg unprocessed wafers for easier processing 
   for wafer in wafers:
     for i in range(1,wafer['quantity']+1):
-      wafers_unprocessed.append({"wafer_id":wafer['type']+"-"+str(i),"processing_times":copy.deepcopy(wafer["processing_times"])})
-  for i in wafers_unprocessed:
-    print(i)
+      wafers_unprocessed.append([{"wafer_id":wafer['type']+"-"+str(i),"processing_times":copy.deepcopy(wafer["processing_times"])},0])
   available_machines  = [] # Each machine has a time from which it is available
   cooldown_machines   = [] # Each machine has a time when it went into cooldown
   processing_machines = [] # Each machine has a wafer id with it
@@ -95,11 +95,11 @@ def scheduleWafers(steps,machines,wafers):
       cooldown_machines.append([machine,0])
     else:
       available_machines.append([machine,0])
-  # print("Available Machines :",available_machines)
-  # print("Cooldown Machines  : ",cooldown_machines)
+  # # print("Available Machines :",available_machines)
+  # # print("Cooldown Machines  : ",cooldown_machines)
 
   while(wafers_unprocessed): # Keeps processing wafers until there are no unprocessed wafers
-    print("\n---------------Processing----------------")
+    # print("\n---------------Processing----------------")
     # For each available machine find a wafer if possible then allocate
     for idx,available_machine in enumerate(available_machines):
       canAssign , wafer_to_assign = findWafer(available_machine[0],wafers_unprocessed,steps)
@@ -110,14 +110,14 @@ def scheduleWafers(steps,machines,wafers):
     # Update the processing_machine list
     for idx,processing_machine in enumerate(processing_machines):
       modified_machine , new_available_time,modified_wafer,mw_schedule = machineProcess(processing_machine)
-      print("Wafers status after modification : ",wafers_unprocessed)
+      # print("Wafers status after modification : ",wafers_unprocessed)
       if(checkIfNeedCooldown(machine,steps)):
         cooldown_machines.append([modified_machine,new_available_time])
       else:
         available_machines.append([modified_machine,new_available_time])
       # If wafer is done then send it to processed
-      if(modified_wafer['processing_times']):
-        print("Appending non empty wafer",modified_wafer)
+      if(modified_wafer[0]['processing_times']):
+        # print("Appending non empty wafer",modified_wafer)
         wafers_unprocessed.append(modified_wafer)
       else :
         wafers_processed.append(modified_wafer)
@@ -129,7 +129,7 @@ def scheduleWafers(steps,machines,wafers):
       modified_machine , new_available_time = machineCooldown(cooldown_machine,machines)
       available_machines.append([modified_machine,new_available_time])
     cooldown_machines = []
-  # print(wafers_unprocessed)
+  # # print(wafers_unprocessed)
   return schedule
 
 def optimalMapping(milestone):
@@ -138,13 +138,13 @@ def optimalMapping(milestone):
   wafers = milestone['wafers']
   solution = {}
   solution['schedule'] = scheduleWafers(steps,machines,wafers)
-  print(solution)    
+  # print(solution)    
   return solution
 
 def readFromFile(milestone_name):
   with open("Workshop_Problem/MilestoneInputs/"+milestone_name+".json","r") as inputfile:
     jsonload = json.load(inputfile)
-  # print(jsonload)
+  # # print(jsonload)
   return jsonload
 
 def writeToFile(sol_for_milestone,milestone_name):
@@ -158,14 +158,14 @@ def runForMilestone(milestone_name):
 
 
 runForMilestone("Milestone0")
-# runForMilestone("Milestone1")
-# runForMilestone("Milestone2a")
-# runForMilestone("Milestone2b")
-# runForMilestone("Milestone3a")
-# runForMilestone("Milestone3b")
-# runForMilestone("Milestone3c")
-# runForMilestone("Milestone4a")
-# runForMilestone("Milestone4b")
-# runForMilestone("Milestone4c")
-# runForMilestone("Milestone5a")
-# runForMilestone("Milestone5b")
+runForMilestone("Milestone1")
+runForMilestone("Milestone2a")
+runForMilestone("Milestone2b")
+runForMilestone("Milestone3a")
+runForMilestone("Milestone3b")
+runForMilestone("Milestone3c")
+runForMilestone("Milestone4a")
+runForMilestone("Milestone4b")
+runForMilestone("Milestone4c")
+runForMilestone("Milestone5a")
+runForMilestone("Milestone5b")
