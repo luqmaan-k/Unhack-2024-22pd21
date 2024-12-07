edbf0fbedda8cf82275b7f45ccb5ad9f979d9429
Luqmaan-k <2luqmaank2004@gmail.com>
2024-12-07 12:01:37 +0530

    Setup the lists for easy processing

--- sol.py
+++ sol.py
@@ -1,26 +1,67 @@
 import json
 
+def writeSchedulePlan():
+  wafers_schedule = []
+  for i in range(1,3):
+    schedule_mapping = {}
+    schedule_mapping['wafer_id'] = "W1"+"-"+ str(i)
+    schedule_mapping['step'] = "S" + str(i)
+    schedule_mapping['machine'] = "M" + str(i)
+    schedule_mapping['start_time'] = 5
+    schedule_mapping['end_time'] = 5
+    wafers_schedule.append(schedule_mapping)
+  return wafers_schedule
+
+# Check whether the machine needs a cooldown
+def checkIfNeedCooldown(machine,steps):
+  for step in steps:
+    if(step['id'] == machine['step_id']):
+      if(machine['initial_parameters']['P1'] in range(step['parameters']['P1'][0],step['parameters']['P1'][1]+1)):
+        return False
+      else:
+        return True
+  # Handle dependencies in other function? step['dependency']
+
+def scheduleWafers(steps,machines,wafers):
+  schedule = {}
+  wafers_unprocessed = []
+  wafers_processing  = []
+  wafers_processed   = []
+  
+  # Covert the wafers to their wafer ids and store in an list containg unprocessed wafers for easier processing 
+  for wafer in wafers:
+    for i in range(1,wafer['quantity']+1):
+      wafers_unprocessed.append({"wafer_id":wafer['type']+"-"+str(i),"processing_times":wafer["processing_times"]})
+  available_machines  = [] # Each machine has a time from which it is available
+  cooldown_machines   = [] # Each machine has a time when it went into cooldown
+  processing_machines = [] # Each machine has a wafer id in it
+  # Initialise the machines
+  for machine in machines:
+    if(checkIfNeedCooldown(machine,steps)):
+      cooldown_machines.append([machine,0])
+    else:
+      available_machines.append([machine,0])
+  # print("Available Machines :",available_machines)
+  # print("Cooldown Machines  : ",cooldown_machines)
+
+  while(wafers_unprocessed): # Keeps processing wafers until there are no unprocessed wafers
+    print("Processing")
+    
+  # print(wafers_unprocessed)
+  return writeSchedulePlan()
+
 def optimalMapping(milestone):
-    steps = milestone['steps']
-    machines = milestone['machines']
-    wafers = milestone['wafers']
-    solution = {}
-    wafers_schedule = []
-    for i in range(1,3):
-      schedule_mapping = {}
-      schedule_mapping['wafer_id'] = "W1"+"-"+ str(i)
-      schedule_mapping['step'] = "" + str(i)
-      schedule_mapping['machine'] = "M" + str(i)
-      schedule_mapping['start_time'] = 5
-      schedule_mapping['end_time'] = 5
-      wafers_schedule.append(schedule_mapping)
-    solution['schedule'] = wafers_schedule    
-    return solution
+  steps = milestone['steps']
+  machines = milestone['machines']
+  wafers = milestone['wafers']
+  solution = {}
+  solution['schedule'] = scheduleWafers(steps,machines,wafers)    
+  return solution
 
 def readFromFile(milestone_name):
   with open("Workshop_Problem/MilestoneInputs/"+milestone_name+".json","r") as inputfile:
     jsonload = json.load(inputfile)
-  print(jsonload)
+  # print(jsonload)
   return jsonload
 
 def writeToFile(sol_for_milestone,milestone_name):
@@ -28,20 +69,20 @@ def writeToFile(sol_for_milestone,milestone_name):
     json.dump(sol_for_milestone,outputfile,indent=4,separators=(",",":"))
 
 def runForMilestone(milestone_name):
-    milestone = readFromFile(milestone_name)
-    sol_for_milestone = optimalMapping(milestone)
-    writeToFile(sol_for_milestone,milestone_name)
+  milestone = readFromFile(milestone_name)
+  sol_for_milestone = optimalMapping(milestone)
+  writeToFile(sol_for_milestone,milestone_name)
 
 
 runForMilestone("Milestone0")
-runForMilestone("Milestone1")
-runForMilestone("Milestone2a")
-runForMilestone("Milestone2b")
-runForMilestone("Milestone3a")
-runForMilestone("Milestone3b")
-runForMilestone("Milestone3c")
-runForMilestone("Milestone4a")
-runForMilestone("Milestone4b")
-runForMilestone("Milestone4c")
-runForMilestone("Milestone5a")
-runForMilestone("Milestone5b")
+# runForMilestone("Milestone1")
+# runForMilestone("Milestone2a")
+# runForMilestone("Milestone2b")
+# runForMilestone("Milestone3a")
+# runForMilestone("Milestone3b")
+# runForMilestone("Milestone3c")
+# runForMilestone("Milestone4a")
+# runForMilestone("Milestone4b")
+# runForMilestone("Milestone4c")
+# runForMilestone("Milestone5a")
+# runForMilestone("Milestone5b")
